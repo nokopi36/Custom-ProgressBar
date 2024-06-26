03cfcb905e20cc2305118c42303395e23c4fe2eb
GitHub Action <action@github.com>
2024-06-26 13:27:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/nokopi36/customprogressbar/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/nokopi36/customprogressbar/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.nokopi36.customprogressbar.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nokopi36.customprogressbar.MyBundle
+import com.github.nokopi36.customprogressbar.services.MyProjectService
 import javax.swing.JButton
 
 
